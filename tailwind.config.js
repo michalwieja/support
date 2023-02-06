acac1a2cc2d64e9ec708909b92ee713b9690111c
Michal Wieja <m.wieja@qarbon.it>
2023-02-06 12:54:35 +0100

    config 7

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#245CE7'
+      }
+    },
+  },
+  plugins: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}']
+};

8f96be98aebd3f769d255e9e34708ddba0f1cfc4
mwieja <mwieja@yahoo.com>
2023-04-11 15:59:02 +0200

    build fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,18 @@
 import '../styles/globals.scss';
 import Head from 'next/head.js';
-import { Header } from '../components/Header/Header.jsx';
+import {Header} from '../components/Header/Header.jsx';
 
-export default function App({
-  Component,
-  pageProps
-}) {
-  return (
+const App = ({Component, pageProps}) => (
     <>
-      <Head>
-        <title>SupPort - Nowa strona wody</title>
-        <meta name="description" content="nowa strona wody"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-      <Header/>
-      <Component {...pageProps} />
+        <Head>
+            <title>SupPort - Nowa strona wody</title>
+            <meta name="description" content="nowa strona wody"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Header/>
+        <Component {...pageProps} />
     </>
-  );
-}
+)
+
+export default App

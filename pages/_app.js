0f355a11eb17ef802b7783c368e00f17ba2395ef
mwieja <m.wieja@qarbon.it>
2023-06-05 11:24:54 +0200

    og tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
 import '../styles/globals.scss';
 import Head from 'next/head.js';
-import {Header} from '../components/Header/Header.jsx';
+import { Header } from '../components/Header/Header.jsx';
 
-const App = ({Component, pageProps}) => (
-    <>
-        <Head>
-            <title>SupPort - Nowa strona wody</title>
-            <meta name="description" content="nowa strona wody"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link rel="icon" href="/favicon.ico"/>
-        </Head>
-        <Header/>
-        <Component {...pageProps} />
-    </>
+const App = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Support - Lepsza strona wody</title>
+      <meta name="description" content="Lepsza strona wody" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta property="og:title" content="Social Title for Cool Page" />
+      <meta
+        property="og:description"
+        content="Support - Lepsza strona wody"
+      />
+      <meta
+        property="og:image"
+        content="oboz-2.jpg"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header />
+    <Component {...pageProps} />
+  </>
 )
 
 export default App
